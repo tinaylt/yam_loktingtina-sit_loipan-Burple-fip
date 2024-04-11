b8746dea81464b9bd51b24311f6b67df87fe9e1d
tinaylt <144871562+tinaylt@users.noreply.github.com>
2024-04-11 01:36:17 -0400

    added search bar in mobile, tablet and desktop size.
    added html, css styling and javascript for the search bar to facilitate different screen size.

--- js/main.js
+++ js/main.js
@@ -5,29 +5,32 @@
 	let button = document.querySelector("#button");
 	let burgerCon = document.querySelector("#burger-con");
     
+    let searchBut = document.querySelector("#searchBarBut");
+    let searchCon = document.querySelector("#searchExtend");
+    
     const buttons = document.querySelectorAll("#download-gallery li");
     const changeImg = document.querySelector("#img");
     
+    
+
 	function hamburgerMenu() {
 		// burgerCon.classList.toggle("slide-toggle");
 		button.classList.toggle("expanded");
         burgerCon.classList.toggle("hidden");
 
-        window.addEventListener("beforeunload", function() {
-            // Close the burger menu
-            button.classList.remove("expanded");
-            burgerCon.classList.add("hidden");
-        });
 	};
+
+    function searchBar() {
+        searchCon.classList.toggle("hidden");
+    };
 
     function changePic(e) {	
         changeImg.src=`images/${e.currentTarget.dataset.downloads}.jpg`	
-    }	
+    };	
     
 
-
-
 	button.addEventListener("click", hamburgerMenu);	
+    searchBut.addEventListener("click", searchBar);
     buttons.forEach(button => button.addEventListener("click", changePic));
 
     
